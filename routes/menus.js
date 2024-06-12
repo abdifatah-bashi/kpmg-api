e321267e719a6b460d4f0c74adaeac29e33962bf
Abdifatah Bashi <abdifatah.bashi@nav.no>
2024-06-12 09:52:23 +0200

    Fiks menus bug

--- routes/menus.js
+++ routes/menus.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const Menu = require('../models/menu');
+const upload = require('../middleware/upload');
 
-router.post('/', async (req, res) => {
+router.post('/', upload.single('image'), async (req, res) => {
   try {
     const menu = new Menu({
       category: req.body.category,
